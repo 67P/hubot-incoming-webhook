378ee4be053a160ab838a63daac61b1ab8541768
Greg Karékinian <greg@karekinian.com>
2018-03-27 12:20:14 +0200

    Remove the comment headers

--- scripts/uptimerobot.js
+++ scripts/uptimerobot.js
@@ -1,19 +1,9 @@
-// Description:
-//    Send a message from a UptimeRobot webhook
-//    You need to add the room attribute to your hook (for example
-//    ops@muc.example.com)
+// Send a message from a UptimeRobot webhook
+// You need to add the room attribute to your hook (for example
+// ops@muc.example.com)
 //
-//     Example UptimeRobot webhook (the ampersand is required):
-//     https://example.com/incoming/uptimerobot/secret?room=ops@example.com&
-//
-//     Example request:
-//     https://example.com/incoming/uptimerobot/secret?room=ops@example.com&monitorFriendlyName=Main%20site&alertTypeFriendlyName=Up&monitorID=23779464475
-//
-// Configuration:
-//   WEBHOOK_TOKEN: A string for building your secret webhook URL
-//
-// Authors:
-//   Greg Karékinian <greg@5apps.com>
+//  Example UptimeRobot webhook (the ampersand is required):
+//  https://example.com/incoming/uptimerobot/secret?room=ops@example.com&
 
 (function () {
   "use strict";
