9562fd99460e15ee60104fbbf95e12871096ed31
Greg Karékinian <greg@karekinian.com>
2018-03-28 15:43:25 +0200

    HTML encode the special characters in the Monitor friendly name
    This is what UptimeRobot does

--- test/uptimerobot.js
+++ test/uptimerobot.js
@@ -12,7 +12,7 @@ const helper = new Helper('../scripts/uptimerobot.js')
 describe('UptimeRobot webhook', function() {
   beforeEach(function(done) {
     this.room = helper.createRoom();
-    const queryStrings = `room=${this.room.name}&monitorFriendlyName=Test+%28staging%29&monitorID=1234567&alertTypeFriendlyName=Down`
+    const queryStrings = `room=${this.room.name}&monitorFriendlyName=Test+%26%2340%3Bstaging%26%2341%3B&monitorID=1234567&alertTypeFriendlyName=Down`
     const hook_uri = `http://localhost:${process.env.EXPRESS_PORT}/incoming/uptimerobot/${process.env.WEBHOOK_TOKEN}?${queryStrings}`;
     http.get(hook_uri, response => {
       this.response = response;
