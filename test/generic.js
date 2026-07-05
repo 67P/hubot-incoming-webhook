602679d5c42c10c0900c56214b6a032f155d1413
Râu Cao <raucao@kosmos.org>
2026-07-05 16:06:44 +0200

    Update dependencies, modernize linting and tests

--- test/generic.js
+++ test/generic.js
@@ -1,54 +1,108 @@
-"use strict";
+'use strict';
 
-const Helper = require('hubot-test-helper')
-const expect = require('chai').expect;
-const http = require('http');
+const Helper = require('hubot-test-helper');
+const { expect } = require('chai');
+const request = require('supertest');
 
-process.env.EXPRESS_PORT = 18080
-process.env.WEBHOOK_TOKEN = "webhooktoken"
+process.env.WEBHOOK_TOKEN = 'webhooktoken';
 
-const helper = new Helper('../hooks/generic.js')
+const helper = new Helper('../hooks/generic.js');
 
-describe('Generic webhook', function() {
-  beforeEach(function(done) {
+describe('Generic webhook', function () {
+  beforeEach(function () {
     this.room = helper.createRoom();
-    const payload = JSON.stringify({ "room": this.room.name, "message": "Good news everyone!" });
-    const options = {
-      hostname: "localhost",
-      port: process.env.EXPRESS_PORT,
-      path: `/incoming/${process.env.WEBHOOK_TOKEN}`,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-
-    const req = http.request(options, response => {
-      this.response = response;
-      done();
-    }).on('error', done);
-
-    req.write(payload);
-    req.end();
   });
 
-  afterEach(function() {
+  afterEach(function () {
     this.room.destroy();
   });
 
-  it('responds with status 200', function() {
-    expect(this.response.statusCode).to.equal(200);
+  it('responds with status 200 and sends a message to the chosen room', async function () {
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/json')
+      .send({ room: this.room.name, message: 'Good news everyone!' });
+
+    expect(res.status).to.equal(200);
+    expect(res.body).to.eql({ ok: true });
+    expect(this.room.messages).to.eql([
+      ['hubot', 'Good news everyone!'],
+    ]);
   });
 
-  it('sends a message to the chosen room', function() {
-    const expected_messages = [
-      [
-        "hubot",
-        "Good news everyone!"
-      ]
-    ]
-    expect(this.room.messages).to.eql(expected_messages);
+  it('sends each line when message is an array', async function () {
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/json')
+      .send({ room: this.room.name, message: ['line one', 'line two'] });
+
+    expect(res.status).to.equal(200);
+    expect(this.room.messages).to.eql([
+      ['hubot', 'line one'],
+      ['hubot', 'line two'],
+    ]);
   });
 
+  it('responds with 422 when room is missing', async function () {
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/json')
+      .send({ message: 'hello' });
+
+    expect(res.status).to.equal(422);
+    expect(this.room.messages).to.eql([]);
+  });
+
+  it('responds with 422 when message is missing', async function () {
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/json')
+      .send({ room: this.room.name });
+
+    expect(res.status).to.equal(422);
+    expect(this.room.messages).to.eql([]);
+  });
+
+  it('responds with 422 when message is an unsupported type', async function () {
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/json')
+      .send({ room: this.room.name, message: 42 });
+
+    expect(res.status).to.equal(422);
+    expect(this.room.messages).to.eql([]);
+  });
+
+  it('accepts a form-encoded payload string', async function () {
+    const payload = JSON.stringify({ room: this.room.name, message: 'via form' });
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send(`payload=${encodeURIComponent(payload)}`);
+
+    expect(res.status).to.equal(200);
+    expect(this.room.messages).to.eql([
+      ['hubot', 'via form'],
+    ]);
+  });
+
+  it('responds with 400 on invalid JSON payload', async function () {
+    const res = await request(this.room.robot.router)
+      .post(`/incoming/${process.env.WEBHOOK_TOKEN}`)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send('payload={not json');
+
+    expect(res.status).to.equal(400);
+    expect(this.room.messages).to.eql([]);
+  });
+
+  it('responds with 404 for an unknown token', async function () {
+    const res = await request(this.room.robot.router)
+      .post('/incoming/wrong-token')
+      .set('Content-Type', 'application/json')
+      .send({ room: this.room.name, message: 'hello' });
+
+    expect(res.status).to.equal(404);
+    expect(this.room.messages).to.eql([]);
+  });
 });
-
--- test/uptimerobot.js
+++ test/uptimerobot.js
@@ -1,42 +1,73 @@
-"use strict";
+'use strict';
 
-const Helper = require('hubot-test-helper')
-const expect = require('chai').expect;
-const http = require('http');
+const Helper = require('hubot-test-helper');
+const { expect } = require('chai');
+const request = require('supertest');
 
-process.env.EXPRESS_PORT = 18080
-process.env.WEBHOOK_TOKEN = "webhooktoken"
+process.env.WEBHOOK_TOKEN = 'webhooktoken';
 
-const helper = new Helper('../hooks/uptimerobot.js')
+const helper = new Helper('../hooks/uptimerobot.js');
 
-describe('UptimeRobot webhook', function() {
-  beforeEach(function(done) {
+describe('UptimeRobot webhook', function () {
+  beforeEach(function () {
     this.room = helper.createRoom();
-    const queryStrings = `room=${this.room.name}&monitorFriendlyName=Test+%26%2340%3Bstaging%26%2341%3B&monitorID=1234567&alertTypeFriendlyName=Down`
-    const hook_uri = `http://localhost:${process.env.EXPRESS_PORT}/incoming/uptimerobot/${process.env.WEBHOOK_TOKEN}?${queryStrings}`;
-    http.get(hook_uri, response => {
-      this.response = response;
-      done();
-    }).on('error', done);
   });
 
-  afterEach(function() {
+  afterEach(function () {
     this.room.destroy();
   });
 
-  it('responds with status 200', function() {
-    expect(this.response.statusCode).to.equal(200);
+  it('responds with status 200 and sends a formatted message', async function () {
+    const qs = new URLSearchParams({
+      room: this.room.name,
+      monitorFriendlyName: 'Test &#40;staging&#41;',
+      monitorID: '1234567',
+      alertTypeFriendlyName: 'Down',
+    }).toString();
+
+    const res = await request(this.room.robot.router)
+      .get(`/incoming/uptimerobot/${process.env.WEBHOOK_TOKEN}?${qs}`);
+
+    expect(res.status).to.equal(200);
+    expect(res.body).to.eql({ ok: true });
+    expect(this.room.messages).to.eql([
+      ['hubot', '[UptimeRobot] Test (staging) is down - https://uptimerobot.com/dashboard.php#1234567'],
+    ]);
   });
 
-  it('sends a message to the chosen room', function() {
-    const expected_messages = [
-      [
-        "hubot",
-        "[UptimeRobot] Test (staging) is down - https://uptimerobot.com/dashboard.php#1234567"
-      ]
-    ]
-    expect(this.room.messages).to.eql(expected_messages);
+  it('responds with 422 when room is missing', async function () {
+    const qs = new URLSearchParams({
+      monitorFriendlyName: 'Test',
+      monitorID: '1234567',
+      alertTypeFriendlyName: 'Down',
+    }).toString();
+
+    const res = await request(this.room.robot.router)
+      .get(`/incoming/uptimerobot/${process.env.WEBHOOK_TOKEN}?${qs}`);
+
+    expect(res.status).to.equal(422);
+    expect(this.room.messages).to.eql([]);
   });
 
+  it('responds with 422 when monitorID is missing', async function () {
+    const qs = new URLSearchParams({
+      room: this.room.name,
+      monitorFriendlyName: 'Test',
+      alertTypeFriendlyName: 'Down',
+    }).toString();
+
+    const res = await request(this.room.robot.router)
+      .get(`/incoming/uptimerobot/${process.env.WEBHOOK_TOKEN}?${qs}`);
+
+    expect(res.status).to.equal(422);
+    expect(this.room.messages).to.eql([]);
+  });
+
+  it('responds with 404 for an unknown token', async function () {
+    const res = await request(this.room.robot.router)
+      .get('/incoming/uptimerobot/wrong-token');
+
+    expect(res.status).to.equal(404);
+    expect(this.room.messages).to.eql([]);
+  });
 });
-
